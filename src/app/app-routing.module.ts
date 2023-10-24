cfa5c630a605594e933e5e299f7e62a62a96d5ed
h-manuel-sekoia <h.manuel@sekoia.ch>
2023-10-24 15:11:21 +0200

    Configure routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { RestaurantdashComponent } from './restaurantdash/restaurantdash.component';
+import { MaterialComponent } from './material/material.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: '**', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'restaurant', component: RestaurantdashComponent},
+  {path: 'material', component: MaterialComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
